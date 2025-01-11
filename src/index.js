06ec487f6c9bf87026d81571bc9b89c5b97bb494
AbelMuro <abelmuro93@gmail.com>
2025-01-11 12:05:12 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const rootRef = document.getElementById('root');
+const root = ReactDOM.createRoot(rootRef);
+root.render(<>Hello WOrld</>)
