4dee642f5f5de506cc634ac39bc420c7b21d166d
abelmuro <abelmuro93@gmail.com>
2025-03-21 14:10:20 -0700

    commit

--- src/Pages/FindPlayers/FindPlayers.js
+++ src/Pages/FindPlayers/FindPlayers.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {useEffect, useMemo} from 'react';
 import DisplayChallenger from './DisplayChallenger';
 import {useSelector} from 'react-redux';
 import * as styles from './styles.module.css';
