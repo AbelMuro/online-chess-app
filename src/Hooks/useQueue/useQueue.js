5624c438f3933785dcca70fb688953adfff39498
abelmuro <abelmuro93@gmail.com>
2025-03-23 13:09:38 -0700

    commit

--- src/Hooks/useQueue/useQueue.js
+++ src/Hooks/useQueue/useQueue.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react';
 
-const WEBSOCKET_URL = 'wss//world-class-chess-server.netlify.app/'  
+const WEBSOCKET_URL = 'wss//world-class-chess-server-image-880168737393.us-central1.run.app/'  
 
 function useQueue() {
     const [queue, setQueue] = useState([]);
