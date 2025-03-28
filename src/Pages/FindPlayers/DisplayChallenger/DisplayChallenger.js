82fddac70bff880b25e006b7dce07f66b5ed58b3
abelmuro <abelmuro93@gmail.com>
2025-03-28 13:59:05 -0700

    commit

--- src/Pages/FindPlayers/DisplayChallenger/DisplayChallenger.js
+++ src/Pages/FindPlayers/DisplayChallenger/DisplayChallenger.js
@@ -7,7 +7,7 @@ function DisplayChallenger({username, image}) {
 
     const handleChallenge = async () => {
         try{
-            const response = await fetch('https://world-class-chess-server.com/create_new_challenge', {
+            const response = await fetch('https://world-class-chess-server.com/send_invitation', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
