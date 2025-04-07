a700aa964a87cf439578227d6d01e0be8c9e54e6
abelmuro <abelmuro93@gmail.com>
2025-04-07 13:40:45 -0700

    commit

--- src/Pages/FindPlayers/DisplayCurrentChallenge/DisplayCurrentChallenge.js
+++ src/Pages/FindPlayers/DisplayCurrentChallenge/DisplayCurrentChallenge.js
@@ -25,6 +25,7 @@ import {motion, AnimatePresence} from 'framer-motion';
 // i may need to debug the challenge websocket somehow
 // i got a websocket error when i did the steps above before
 
+//also, look into WebRTC in react with socket.io
 
 const callbackForChallengeWebSocket = (navigate, dispatch, setChallenger) => {
 
