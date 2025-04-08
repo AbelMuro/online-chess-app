80527e482aa46f2872cb7dd8ed0a558cb5a8e0fe
AbelMuro <abelmuro93@gmail.com>
2025-04-08 15:47:13 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Store';
@@ -14,22 +14,6 @@ import DisplayMessage from './assets/Components/DisplayMessage';
 import './global.css';
 
 function App() {
-
-    useEffect(() => {
-        const fetchFunction = async () => {
-            try{
-                const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-                const result = await response.json();
-                console.log(result);
-            }
-            catch(error){
-                const message = error.message;
-                console.log(message);
-            }
-        };
-
-        fetchFunction();
-    }, [])
 
     return(
         <Provider store={store}>
