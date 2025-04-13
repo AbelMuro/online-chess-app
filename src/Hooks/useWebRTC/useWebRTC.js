d4ae5b0bddb4b4f58a4910ea36c1574a21de6b52
abelmuro <abelmuro93@gmail.com>
2025-04-12 17:19:36 -0700

    commit

--- src/Hooks/useWebRTC/useWebRTC.js
+++ src/Hooks/useWebRTC/useWebRTC.js
@@ -23,6 +23,7 @@ function useWebRTC(){
         const dataChannel = peerConnection.createDataChannel('chat');
         dataChannel.onopen = () => console.log('Data channel open');
         dataChannel.onmessage = (e) => console.log('Received: ', e.data);
+        dataChannel.onclose = () => {console.log('Data channel closed')}
  
         peerConnection.onicecandidate = event => {
             if(event.candidate) 
@@ -42,7 +43,6 @@ function useWebRTC(){
                 const text = await message.data.text();
                 const data = JSON.parse(text);
                 console.log(data);
-                console.log(peerConnection.signalingState);
             
                 if(data.type === 'offer' && peerConnection.signalingState === 'stable') {                                                            //we handle a connection here (when a remote client wants to connect to a local client)
                     await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));   //we create a remote description of the offer  (remote description are the connection settings of the OTHER peer)
