4a123998c3d0dcec5ad39cc60cf661ef2109e1b5
abelmuro <abelmuro93@gmail.com>
2025-05-03 15:14:06 -0700

    e

--- src/Store/Reducers/WebRtcReducer/Middleware/createDataChannel.js
+++ src/Store/Reducers/WebRtcReducer/Middleware/createDataChannel.js
@@ -1,14 +1,15 @@
 import { connectionManager } from "../WebRtcReducer.js";
 
 
-const createDataChannel = async (_, {dispatch}) => {
+const createDataChannel = (_, {dispatch}) => {
     try{
         const peerConnection = connectionManager.peerConnection;      
 
-        return new Promise((resolved, rejected) => {
-            const dataChannel = peerConnection.createDataChannel('chat');     
+        return new Promise((resolve, reject) => {
+            const dataChannel = peerConnection.createDataChannel('chat');    
+            console.log('inside the promise'); 
             dataChannel.onopen = () => {
-                resolved({dataChannel});  
+                resolve({dataChannel});  
             };
             dataChannel.onclose = () => {
                 dispatch({type: 'CLOSE_DATA_CHANNEL'})
@@ -17,7 +18,7 @@ const createDataChannel = async (_, {dispatch}) => {
                 const message = error.message;
                 console.log('Local data channel experienced an error ', message);
                 dispatch({type: 'SET_ERROR', payload: {message }});
-                rejected({message});
+                reject({message});
             };
             dataChannel.onmessage = (e) => {
                 console.log('Received message from remote client', e.data)
