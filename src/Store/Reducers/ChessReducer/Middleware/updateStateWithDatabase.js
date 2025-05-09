706f6223b52926883364c140ef087b8882bc802c
abelmuro <abelmuro93@gmail.com>
2025-05-09 12:30:52 -0700

    commit

--- src/Store/Reducers/ChessReducer/Middleware/updateStateWithDatabase.js
+++ src/Store/Reducers/ChessReducer/Middleware/updateStateWithDatabase.js
@@ -26,6 +26,8 @@ const updateStateWithDatabase = async (matchId, {dispatch}) => {
             const playerOneColor = playerOne.color;
             const playerTwoColor = playerTwo.color;
 
+            console.log(playerOne.username, playerTwo.username);
+
             dispatch({type: 'SET_GAME_SETTINGS', payload: {
                 user: localClientUsername === playerOne.username ? playerOneColor : playerTwoColor,
                 opponent: localClientUsername === playerOne.username ? playerTwoColor : playerOneColor,
