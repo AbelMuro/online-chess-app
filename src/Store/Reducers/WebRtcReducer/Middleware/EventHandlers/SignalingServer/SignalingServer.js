0f7bf522f57ae8007ce10249479abce3db526a1e
abelmuro <abelmuro93@gmail.com>
2025-05-11 14:57:15 -0700

    commit

--- src/Store/Reducers/WebRtcReducer/Middleware/EventHandlers/SignalingServer/SignalingServer.js
+++ src/Store/Reducers/WebRtcReducer/Middleware/EventHandlers/SignalingServer/SignalingServer.js
@@ -7,6 +7,7 @@ const signalingServerOnMessage = (peerConnection, dispatch, signalingServer, get
             const text = await e.data.text();
             const data = JSON.parse(text);
             const remoteClientUsername = data.from;
+            console.log('remote client username', remoteClientUsername, data);
         
             if(data.type === 'offer' && peerConnection.signalingState === 'stable') {                                                            //we handle a connection here (when a remote client wants to connect to a local client)
                 await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));   //we create a remote description of the offer  (remote description are the connection settings of the OTHER peer)
