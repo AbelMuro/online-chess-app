ac19dc175c83e531fba3a2748e186afc67bf061b
abelmuro <abelmuro93@gmail.com>
2025-05-10 17:18:40 -0700

    commit

--- src/Hooks/useWebSocket/useWebSocket.js
+++ src/Hooks/useWebSocket/useWebSocket.js
@@ -1,39 +1,30 @@
-import {useState, useEffect, useRef} from 'react';
+import {useState, useEffect} from 'react';
 
 
-function useWebSocket(url, callback, initialState, dataToRemove) {
+function useWebSocket(url, callback, initialState) {
     const [data, setData] = useState(initialState);
-    const socketRef = useRef();
-    const [ready, setReady] = useState(false);
 
     useEffect(() => {
-        socketRef.current = new WebSocket(url);            	
+        const socket = new WebSocket(url);            	
 
-        socketRef.current.onopen = () => {                                        
+        socket.onopen = () => {                                        
             console.log(`Connected to ${url} websocket servers`);
-            setReady(true);
         };
     
-        socketRef.current.onmessage = callback;                        
+        socket.onmessage = callback;                        
     
-        socketRef.current.onclose = () => {
+        socket.onclose = () => {
             console.log(`Disconnected from ${url} websocket server`);
         };
     
-        socketRef.current.onerror = (error) => {
+        socket.onerror = (error) => {
             console.error(`Error occurred in websocket ${url}: `, error);
         };
 
         return () => {
-            socketRef.current?.close?.();
+            socket?.close?.();
         }
     }, [])
-
-    useEffect(() => {
-        if(!ready) return;
-
-        socketRef.current.send(dataToRemove);
-    }, [ready])
 
     return [data, setData];
 }
