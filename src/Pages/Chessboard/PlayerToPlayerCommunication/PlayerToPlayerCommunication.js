ffc5c55d8c4b50ce3a4a8860d1d4d1cf719e4dfb
abelmuro <abelmuro93@gmail.com>
2025-05-16 16:21:02 -0700

    comit

--- src/Pages/Chessboard/PlayerToPlayerCommunication/PlayerToPlayerCommunication.js
+++ src/Pages/Chessboard/PlayerToPlayerCommunication/PlayerToPlayerCommunication.js
@@ -9,8 +9,10 @@ function PlayerToPlayerCommunication({matchId}) {
     const playerTwo = useSelector(state => state.settings.player_two);
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const localClientColor = playerOne.username === localClientUsername ? playerOne.color: playerTwo.color
+    const localClientColor = playerOne.username === localClientUsername ? playerOne.color: playerTwo.color;
+    console.log('local client color = ', localClientColor);
 
+    return;
     useWebSocket(`wss://world-class-chess-server.com:443/match?matchId=${matchId}player=${localClientUsername}color=${localClientColor}`, 
         (e) => {
             console.log('received message from match websocket')
