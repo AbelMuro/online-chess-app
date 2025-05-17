134baa347094ec17dc71d8b7b6699646fcbf8a36
abelmuro <abelmuro93@gmail.com>
2025-05-17 16:00:06 -0700

    commit

--- src/Pages/Chessboard/PlayerToPlayerCommunication/PlayerToPlayerCommunication.js
+++ src/Pages/Chessboard/PlayerToPlayerCommunication/PlayerToPlayerCommunication.js
@@ -6,17 +6,20 @@ import {useSelector, useDispatch} from 'react-redux';
 function PlayerToPlayerCommunication({matchId}) {
     const board = useSelector(state => state.chess.board)
     const localClientUsername = useSelector(state => state.account.username);
+    const currentTurn = useSelector(state => state.chess.current_turn);
+    const userColor = useSelector(state => state.settings.user_color);
     const playerOne = useSelector(state => state.settings.player_one);
     const playerTwo = useSelector(state => state.settings.player_two);
     const dispatch = useDispatch();
     const skipFirstRender = useRef(true);
 
     useEffect(() => {
-        console.log('finalize turn')
         if(skipFirstRender.current) {
             skipFirstRender.current = false;
             return;
         }
+        if(!userColor || currentTurn === userColor) return;
+
 
         const finalizeTurn = async () => {
             dispatch({type: 'RESET_TIMER', payload: {seconds: 60}});
@@ -25,7 +28,7 @@ function PlayerToPlayerCommunication({matchId}) {
 
         finalizeTurn();
 
-    }, [board])
+    }, [board, currentTurn, userColor])
 
     useEffect(() => {
         if(!playerOne?.color || !playerTwo?.color || !playerOne?.username) return;
