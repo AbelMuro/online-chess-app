0f5ad4bc1eed0241f361f31ae74f5c3282d5062c
abelmuro <abelmuro93@gmail.com>
2025-05-18 14:17:56 -0700

    commit

--- src/Hooks/useConfirmNavigation/useConfirmNavigation.js
+++ src/Hooks/useConfirmNavigation/useConfirmNavigation.js
@@ -5,16 +5,16 @@ function useConfirmNavigation(shouldBlock) {
     const block = useBlocker(shouldBlock);
 
     useEffect(() => {
-        if(block !== 'blocked') return;
+        if(block.state !== 'blocked') return;
 
         const confirmed = confirm('Are you sure you want to leave? You will forfeit the match in doing so');
         if(confirmed)
             block.proceed();
         else
             block.reset();
-    }, [block])
+    }, [block.state])
 
-    
+    return block
 }
 
 export default useConfirmNavigation;
