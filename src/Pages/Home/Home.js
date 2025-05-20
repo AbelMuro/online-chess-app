e2938673f3ed990cfa03d278abb7b7a02664de1b
AbelMuro <abelmuro93@gmail.com>
2025-05-19 22:26:22 -0700

    c

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,9 @@ import NavigationBar from './NavigationBar';
 import Header from './Header';
 import * as styles from './styles.module.css';
 
+//this is where i left off, i will need to update my notes on svg, and think
+//of the next component to animate here
+
 function Home() {
     return(
         <section className={styles.home}>
