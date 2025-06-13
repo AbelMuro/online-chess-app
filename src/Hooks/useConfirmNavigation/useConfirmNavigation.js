d575c2b014245321809fb16c5063761715b95f1a
abelmuro <abelmuro93@gmail.com>
2025-06-13 13:59:40 -0700

    commit

--- src/Hooks/useConfirmNavigation/useConfirmNavigation.js
+++ src/Hooks/useConfirmNavigation/useConfirmNavigation.js
@@ -1,15 +1,21 @@
 import {useEffect} from 'react';
-import {useBlocker} from 'react-router-dom';
+import {useBlocker, useDispatch} from 'react-router-dom';
+
+//this is where i left off, i may need to find a more reliable way of reseting the state
 
 function useConfirmNavigation(shouldBlock) {
     const block = useBlocker(shouldBlock);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         if(block.state !== 'blocked') return;
 
         const confirmed = confirm('Are you sure you want to leave? You will forfeit the match in doing so');
-        if(confirmed)
+        if(confirmed){
+            dispatch({type: 'RESET_STATE'});
             block.proceed();
+        }
+            
         else
             block.reset();
     }, [block.state])
