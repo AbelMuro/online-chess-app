754f8de50c78b81ef38a0fb24deedff896efa506
abelmuro <abelmuro93@gmail.com>
2025-06-13 10:47:36 -0700

    commit

--- src/Pages/Chessboard/SideBar/Timer/Timer.js
+++ src/Pages/Chessboard/SideBar/Timer/Timer.js
@@ -9,21 +9,21 @@ function Timer() {
     const {matchId} = useParams();
     if(matchId === 'ai') return null;
     const seconds = useSelector(state => state.timer.seconds);
-    //const stop = useSelector(state => state.timer.stop);
+    const stop = useSelector(state => state.timer.stop);
     const localClientUsername = useSelector(state => state.account.username);
     const currentTurn = useSelector(state => state.chess.current_turn);
     const userColor = useSelector(state => state.settings.user_color)
     const timerRef = useRef();
     const dispatch = useDispatch();
 
-    const displayTimer = async () => {
+    const displayTimer = () => {
         const sec = Math.floor(seconds % 60);
         const min = Math.floor(seconds / 60);
         const formattedSec = sec === 0 ? '00' : sec < 10 ? `0${sec}` : sec;
         if(sec === 0 && min === 0){
             clearInterval(timerRef.current);
             dispatch({type: 'PLAYER_RAN_OUT_OF_TIME', payload: {player: localClientUsername, color: userColor}})
-            await dispatch(syncDatabaseWithState(matchId));
+            dispatch(syncDatabaseWithState(matchId));
             return '0:00';
         }
             
@@ -42,13 +42,13 @@ function Timer() {
         }
     }, [currentTurn, userColor])
 
-/*
+
     useEffect(() => {
         if(!stop) return;
 
         clearInterval(timerRef.current)
     }, [stop])
-*/
+
     return(
         <div className={styles.timer}>
             {displayTimer()}
