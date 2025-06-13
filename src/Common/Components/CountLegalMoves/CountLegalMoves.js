7f12e3861672ed5e828c26a2beabff4f039ec442
abelmuro <abelmuro93@gmail.com>
2025-06-13 12:07:39 -0700

    commit

--- src/Common/Components/CountLegalMoves/CountLegalMoves.js
+++ src/Common/Components/CountLegalMoves/CountLegalMoves.js
@@ -4,12 +4,12 @@ import {useSelector, useDispatch} from 'react-redux';
 function CountLegalMoves({row, column, color, pieceId}){
     const dispatch = useDispatch();
     const board = useSelector(state => state.chess.board);
+    console.log('count legal moves');
 
     useEffect(() => {
         dispatch({type: 'COUNT_LEGAL_MOVES', payload: {square: {row, column, color}}});
         
         return () => {
-            console.log('reset legal moves')
             dispatch({type: 'RESET_LEGAL_MOVES', payload: {pieceId, color}});
         }
 
